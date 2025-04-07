b928c93665d65379cf9cb5c2dc09ee1b4c123891
mdimteyaz6493 <mdimteyaz6493@gmail.com>
2025-04-08 00:13:40 +0530

    links update

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:5002",
+        target: "https://chaapp-re.onrender.com",
         changeOrigin: true,
       },
     },
